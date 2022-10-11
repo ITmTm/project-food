45cadfa8243e1ed4149794843e82087c14b49e0a
Timur <98873757+ITmTm@users.noreply.github.com>
2022-10-11 17:59:42 +0500

    add date

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2022-08-31');
+    timer('.timer', '2022-10-31');
     cards();
     calculator();
     forms('form', modalTimerId);
